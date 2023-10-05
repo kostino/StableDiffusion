9da28d358f1511239c698b6086fa168e4fbb7032
Kostas Triaridis <kost.tri@gmail.com>
2023-10-05 13:46:30 +0300

    add UNET

--- sd/attention.py
+++ sd/attention.py
@@ -48,3 +48,46 @@ class SelfAttention(nn.Module):
         output = self.out_proj(output)
 
         return output
+
+
+class CrossAttention(nn.Module):
+    def __init__(self, n_heads, d_embed, d_cross, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+        self.q_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x, y):
+
+        input_shape = x.shape
+        b, l, d_embed = input_shape
+
+        interm_shape = (b, -1, self.n_heads, self.d_head)
+
+        q = self.q_proj(x)
+        k = self.k_proj(y)
+        v = self.v_proj(y)
+
+        q = q.view(interm_shape).transpose(-1, -2)
+        k = k.view(interm_shape).transpose(-1, -2)
+        v = v.view(interm_shape).transpose(-1, -2)
+
+        weight = q @ k.transpose(-1, -2)
+
+        weight /= math.sqrt(self.d_head)
+
+        weight = F.softmax(weight, dim=-1)
+
+        output = weight @ v
+
+        output = output.transpose(1, 2).contiguous()
+
+        output = output.view(input_shape)
+
+        output = self.out_proj(output)
+
+        return output
